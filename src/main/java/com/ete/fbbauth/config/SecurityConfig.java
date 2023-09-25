7f008c2cb4d948bbd8325c77666a0e2cd9a41811
radhi <radhikabhatt.ec@gmail.com>
2023-09-25 13:57:25 +0300

    initial commit

---
+++ src/main/java/com/ete/fbbauth/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ete.fbbauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+//		uses the widely supported bcrypt algorithm to hash the passwords.
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER")
+				.build();
+		
+		
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+		
+		
+//		we have created two users(user,admin) and stored them in the InMemoryUserDetailsManager class object.
+		return new InMemoryUserDetailsManager(user,admin);
+		
+	}
+	
+}
+
+
